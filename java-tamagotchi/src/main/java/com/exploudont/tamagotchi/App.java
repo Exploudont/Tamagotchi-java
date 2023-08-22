303f6df96390804ab0580f597bbe3ebccb5c0336
Exploudont <33834591+Exploudont@users.noreply.github.com>
2023-08-22 19:24:55 +0200

    Project creation
    Created the project

---
+++ java-tamagotchi/src/main/java/com/exploudont/tamagotchi/App.java
@@ -0,0 +1,11 @@
+package com.exploudont.tamagotchi;
+
+/**
+ * Hello world!
+ */
+public class App {
+	
+	public static void main( String[] args ) {
+		System.out.println("Hello Tamagotchi!");
+	}
+}
